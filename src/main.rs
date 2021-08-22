f890028dd11c29a85ee0669bc03a517b5fed66dd
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-22 22:18:58 +0900

    change to impl

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,15 @@ struct Temperature {
     degrees_f: f64,
 }
 
-fn show_temp(temp: Temperature) {
-    println!("{} degrees F", temp.degrees_f);
+impl Temperature {
+    fn show_temp(temp: Temperature) {
+        println!("{} degrees F", temp.degrees_f);
+    }
 }
 
 fn main() {
     let hot = Temperature {
         degrees_f: 99.9
     };
-    show_temp(hot);
+    Temperature::show_temp(hot);
 }
