12bcde0ada3be8c22fc0d9307628db5e6515aace
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-22 22:16:45 +0900

    basic way

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+struct Temperature {
+    degrees_f: f64,
+}
+
+fn show_temp(temp: Temperature) {
+    println!("{} degrees F", temp.degrees_f);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hot = Temperature {
+        degrees_f: 99.9
+    };
+    show_temp(hot);
 }
