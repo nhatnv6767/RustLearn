6d36000e57f5395beca670c38e221f5c0423045e
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-23 00:44:03 +0900

    function -> Self ; Self with upper case S

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ struct Temperature {
 }
 
 impl Temperature {
+    // Self with "S" can be replaced by Temperature
+    fn freezing() -> Self {
+        Self {
+            degrees_f: 32.0
+        }
+    }
+
+    fn boiling() -> Self {
+        Self {
+            degrees_f: 200.0
+        }
+    }
+
+    // it's borrowing
     fn show_temp(&self) {
         println!("{} degrees F", self.degrees_f);
     }
@@ -13,4 +27,12 @@ fn main() {
         degrees_f: 99.9
     };
     hot.show_temp();
+
+    let cold = Temperature::freezing();
+    cold.show_temp();
+    cold.show_temp();
+
+    let boiling = Temperature::boiling();
+    boiling.show_temp();
+
 }
