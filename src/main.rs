06c9b2df1b31401b6078149f0c1c6ddbabb67470
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-22 22:25:52 +0900

    meaning of &self in function (impl - implement)

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ struct Temperature {
 }
 
 impl Temperature {
-    fn show_temp(temp: Temperature) {
-        println!("{} degrees F", temp.degrees_f);
+    fn show_temp(&self) {
+        println!("{} degrees F", self.degrees_f);
     }
 }
 
@@ -12,5 +12,5 @@ fn main() {
     let hot = Temperature {
         degrees_f: 99.9
     };
-    Temperature::show_temp(hot);
+    hot.show_temp();
 }
