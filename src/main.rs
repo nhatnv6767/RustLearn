2f46c0e4a934a4625f2c004fdbe3b83d167d48ee
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-23 23:30:44 +0900

    Implementing...

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,22 @@
     Implement functionality on the box struct to print the characteristics
 */
 
-// should create enum, and fn first, impl the 2nd and last is main()
+//  Should create enum, and fn first, impl the 2nd and last is main()
 
-// Use an enum for the box color
+//  Use an enum for the box color
 
 enum Color {
     Brown,
     Red
+}
+
+impl Color {
+    fn print(&self) {
+        match self {
+            Color::Brown => println!("Brown"),
+            Color::Red => println!("Red"),
+        }
+    }
 }
 
 struct Dimensions {
@@ -27,13 +36,34 @@ struct Dimensions {
     depth: f64,
 }
 
-// Use a struct to encapsulate the box characteristics
-// Must include dimensions, weight, and color
+impl Dimensions {
+    fn print(&self) {
+        println!("Width: {}", self.width);
+        println!("Height: {}", self.height);
+        println!("Depth: {}", self.depth);
+    }
+}
+
+//  Use a struct to encapsulate the box characteristics
+//  Must include dimensions, weight, and color
 struct ShippingBox {
     color: Color,
     weight: f64,
     dimensions: Dimensions
 }
+
+//  Implement functionality on the box struct to create a new box
+//  Implement functionality on the box struct to print the characteristics
+impl ShippingBox {
+    fn new(weight: f64, color: Color, dimensions: Dimensions) -> Self {
+        Self {
+            weight,
+            color,
+            dimensions,
+        }
+    }
+}
+
 
 fn main() {
 
