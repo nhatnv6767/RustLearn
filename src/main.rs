01ddf5b3f4a0aa93baac4c329bef9d0a65f9433d
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-25 00:40:28 +0900

    example 1 + explain

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,22 @@ Must use an owned String to store in a struct
 Use &str when passing to a function
 
  */
-fn print_it(data: &str) {
-    println!("{}", data);
+
+/*
+Here's an example of a struct, and we're attempting to store a string slice within the struct,
+just mentioned, when you create a string in line 2nd after main() "Nhatnv", it's automatically borrowed
+If we try to store borrowed data and structure, it won't compile
+And the reason is when this structure (Employee) is to be dropped at the end of the scope, the structure is responsible for
+cleaning up its own memory. However, since we have borrowed memory (&str), the structure is not allowed to clean it up because it doesn't own this data
+So it results in a compiler error and it'll inform you that you cannot store a string slice in this
+ */
+struct Employee {
+    name: &str,
 }
 
-fn main() {
-    print_it("a string slice");
-    let owned_string = "owned string".to_owned();
-    let another_owned = String::from("another");
-    print_it(&owned_string);
-    print_it(&another_owned)
+fn main(){
+    let emp_name = "Nhatnv";
+    let emp = Employee {
+        name: emp_name
+    };
 }
