3fd2e017269ea1b68ec647e832d8957e72253fa8
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-25 00:25:53 +0900

    example 1

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ Must use an owned String to store in a struct
 Use &str when passing to a function
 
  */
+fn print_it(data: &str) {
+    println!("{}", data);
+}
+
 fn main() {
-    println!("Hello, world!");
+    print_it("a string slice");
+    let owned_string = "owned string".to_owned();
+    let another_owned = String::from("another");
+    print_it(&owned_string);
+    print_it(&another_owned)
 }
