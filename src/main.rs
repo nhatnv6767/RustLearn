e2262de15bdd662251527f348d34328d917372d4
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-24 23:14:54 +0900

    43 - Vectors & For loops

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+struct Test {
+    score: i32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let my_scores = vec![
+        Test { score: 23 },
+        Test { score: 90 },
+        Test { score: 99 },
+        Test { score: 100 },
+    ];
+
+    for test in my_scores {
+        println!("Score = {}", test.score);
+    }
 }
