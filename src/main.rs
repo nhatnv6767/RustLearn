cd155dcba5b9c71a2e793a9208a205b53abe820a
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-24 23:36:54 +0900

    define

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/*
+
+. String - owned
+. &str - borrowed String slice
+
+Must use an owned String to store in a struct
+Use &str when passing to a function
+
+ */
 fn main() {
     println!("Hello, world!");
 }
