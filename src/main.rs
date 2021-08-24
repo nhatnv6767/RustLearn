5c4cc3317ef4cec9020168cc60cbf7a3db636f8a
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-24 23:29:28 +0900

    done

--- src/main.rs
+++ src/main.rs
@@ -10,3 +10,19 @@
     Use the .len() function to print the number of elements in a vector
 
  */
+
+fn main() {
+    //  Use a vector to store 4 numbers
+    let my_numbers = vec![10, 20, 30, 40];
+    //  Iterate through the vector using a for..in loop
+    // for loop is simply borrowing my numbers
+    for num in &my_numbers {
+        //  Determine whether to print the number or print "thirty" inside
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{}", num),
+        }
+    }
+    //  Use the .len() function to print the number of elements in a vector
+    println!("number of elements = {}", my_numbers.len());
+}
