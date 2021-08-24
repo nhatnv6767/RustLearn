d826ee8914e5c0a6781bf50a7c0bbf9e3c7c0870
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-24 23:19:29 +0900

    Requirement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+/*
+
+    Print 10, 20, "thirty", and 50 in a loop
+    Print the total number of elements in a vector
+
+    Notes:
+    Use a vector to store 4 numbers
+    Iterate through the vector using a for..in loop
+    Determine whether to print the number or print "thirty" inside
+    Use the .len() function to print the number of elements in a vector
+
+ */
