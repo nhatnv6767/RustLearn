f3915e08b93fff1406b3847ce4e005acd06f576b
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-27 01:04:40 +0900

    basic 1

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,41 @@ Notes:
 
 */
 
+
+// * Use a struct for a persons age, name, and favorite color
+// * The color and name should be stored as a String
+struct Person {
+    name: String,
+    fav_color: String,
+    age: i32,
+}
+
 fn main() {
+    // * Create and store at least 3 people in a vector
+    let people = vec![
+        Person {
+            name: String::from("George"),
+            fav_color: String::from("green"),
+            age: 7,
+        },
+        Person {
+            name: String::from("Anna"),
+            fav_color: String::from("purple"),
+            age: 9,
+        },
+        Person {
+            name: String::from("Katie"),
+            fav_color: String::from("blue"),
+            age: 14,
+        },
+    ];
+    // * Iterate through the vector using a for..in loop
+    for person in people {
+        // * Use an if expression to determine which person's info should be printed
+        if person.age <= 10 {
+            // * The name and colors should be printed using a function
+        }
+
+    }
 
 }
