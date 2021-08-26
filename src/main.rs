5adac5c22025cf8131908eabfe91df95d9b9e60a
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-08-27 00:24:40 +0900

    explain

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,10 @@ fn main() {
     ];
 
     for item in receipt {
+        // in the 2nd line is String data type as the name, so this is an owned string
+        // however, print_name function requires a borrowed string slice.
+        // so in here, can't just send in the item name because that is a string, so instead we simply need to borrow the name filed "&item.name"
+        // that mean we were borrowing this filed here, the name field and the 2nd line is going to create a string slice from that (2nd) string
         print_name(&item.name);
         println!("count: {}", item.count);
     }
