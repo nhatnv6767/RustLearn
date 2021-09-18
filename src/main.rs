80ee33198159cb59005f939760152c35750bface
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 20:30:55 +0900

    requirements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// Topic: Option
+//
+// Requirements:
+// * Print out the details of a student's locker assignment
+// * Lockers use numbers and are optional for students
+//
+// Notes:
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+
+// * Use a struct containing the student's name and locker assignment
+// * The locker assignment should use an Option<i32>
+
 fn main() {
     println!("Hello, world!");
 }
