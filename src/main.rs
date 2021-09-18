aa4a1a88db5c9a5ede4800180e86fe3511464585
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 18:51:51 +0900

    example 1 - just read, dont run

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+enum Option<T> {
+    Some(T),
+    None,
+}
+
+struct Customer {
+    age: Option<i32>,
+    email: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mark = Customer {
+        age: Some(22),
+        email: "mark@gmail.com".to_owned(),
+    };
+    let becky = Customer {
+        age: None,
+        email: "becky@gmail.com".to_owned(),
+    };
+
+    match becky.age {
+        Some(age) => println!("Customer is {} years old", age),
+        None => println!("Customer age not provided"),
+    }
 }
