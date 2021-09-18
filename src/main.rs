46f3ccab0abf511c33c6098e8cf4805fc05bff99
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 19:04:44 +0900

    example 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+struct Survey {
+    q1: Option<i32>,
+    q2: Option<bool>,
+    q3: Option<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let response = Survey {
+        q1: Some(12),
+        q2: Some(true),
+        q3: Some("A".to_owned()),
+    };
+    match response.q1 {
+        Some(ans) => println!("q1: {}", ans),
+        None => println!("q1: no response"),
+    }
 }
