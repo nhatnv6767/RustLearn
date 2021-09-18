8bd87c83576c0e3cb0337459fc3d97e6496ddab6
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 22:22:32 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+fn get_sound(name: &str) -> Result<SoundData, String> {
+    if name == "alert" {
+        Ok(SoundData::new("alert")),
+    } else {
+        Err("Unable to find sound data".to_owned())
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let sound = get_sound("alert");
+    match sound {
+        Ok(_) => println!("sound data located"),
+        Err(e) => println!("error: {}", e),
+    }
 }
