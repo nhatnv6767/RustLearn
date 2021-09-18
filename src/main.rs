5f4cd952dbb7b4b3345dea020e577c37e8eef194
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 15:05:21 +0900

    completed

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,36 @@
 // * Use a match expression while iterating the vector to print the ticket info
 
 
+// * Use an enum for the tickets with data associated with each variant
+// * Tickets can be Backstage, Vip, and Standard
+
+// * Backstage and Vip tickets include the ticket holder's name
+// * All tickets include the price
+enum Ticket {
+    // price first and then any additional information
+    Standard(f64, String),
+    Backstage(f64),
+    Vip(f64, String),
+}
+
 fn main() {
-    println!("Hello, world!");
+    // * Create one of each ticket and place into a vector
+    let tickets = vec![
+        // to_owned -> create own the string
+        Ticket::Backstage(50.0, "Billy".to_owned()),
+        Ticket::Standard(15.0),
+        Ticket::Vip(30.0, "Amy".to_owned()),
+    ];
+    // * Use a match expression while iterating the vector to print the ticket info
+    for ticket in tickets {
+        match ticket {
+            Ticket::Backstage(price, holder) => {
+                println!("Backstage ticket holder: {}, price: {}", holder, price)
+            }
+            Ticket::Standard(price) => println!("Standard ticket Price: {}", price),
+            Ticket::Vip(price, holder) => {
+                println!("VIP ticket holder: {}, price: {}", holder, price)
+            }
+        }
+    }
 }
