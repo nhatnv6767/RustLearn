9e99987c6f1cd0ab6437a9dc135a35aca819651e
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 14:09:47 +0900

    finish - 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+struct Person {
+    name: String,
+    fav_color: String,
+    age: i32,
+}
+
+fn print(name: &str, fav_color: &str) {
+    println!("{}: {}", name, fav_color);
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let people = vec![
+        Person {
+            name: String::from("George"),
+            fav_color: String::from("green"),
+            age: 7,
+        },
+
+        Person {
+            name: String::from("NhatNguyen"),
+            fav_color: String::from("blue"),
+            age: 9,
+        },
+
+        Person {
+            name: String::from("Balon"),
+            fav_color: String::from("Yellow"),
+            age: 15,
+        },
+    ];
+
+    for person in people {
+        if person.age <= 10 {
+            print(&person.name, &person.fav_color);
+        }
+    }
 }
