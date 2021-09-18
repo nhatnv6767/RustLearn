d4d784fdd99cd916b6b1771dc7621c03f9fc005c
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-18 22:33:31 +0900

    Description the example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+/*
+be showing how result can be utilized with functions that may possibly fail
+a program that simulates a menu and the mnu will have multiple choices, the user
+will be able to enter some text that corresponds to one of the choices.
+if it fails, then the user will be notified the reason it fails internally
+we're going to use an enumeration for each menu choice and we will utilize a function that
+returns a results in order to transform the user input into the enumeration
+*/
+
+
 fn main() {
     println!("Hello, world!");
 }
