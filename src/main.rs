ea0f7754c89f733338f1d0e982590e4efc26c023
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-19 23:53:01 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,18 @@
 - Similar to definitions in a dictionary
 - Very fast to retrieve data using the key
  */
+use std::collections::HashMap;
+
 fn main() {
-    println!("Hello, world!");
+    let mut people = HashMap::new();
+    people.insert("Susan", 21);
+    people.insert("Ed", 13);
+    people.insert("Will", 14);
+    people.insert("Cathy", 22);
+    people.remove("Susan");
+
+    match people.get("Ed") {
+        Some(age) => println!("age = {:?}", age);
+        None => println!("not found"),
+    }
 }
