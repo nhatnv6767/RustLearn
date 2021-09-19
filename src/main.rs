b11191954c114f6f1c927f1e40b81c4860aec004
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-19 23:49:59 +0900

    define

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*
+- Collection that stores data as key-value pairs
+    - Data is located using the "key"
+    - The data is the "value"
+- Similar to definitions in a dictionary
+- Very fast to retrieve data using the key
+ */
 fn main() {
     println!("Hello, world!");
 }
