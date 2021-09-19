322dd0c046819a725fe05f953b56df011e702576
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-20 00:24:12 +0900

    simple example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::collections::HashMap;
+#[derive(Debug)]
+struct Contents {
+    content: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut lockers = HashMap::new();
+    lockers.insert(
+        1,
+        Contents {
+            content: "stuff".to_string(),
+        },
+    );
+    lockers.insert(
+        2,
+        Contents {
+            content: "shirt".to_string(),
+        },
+    );
+    lockers.insert(
+        3,
+        Contents {
+            content: "gym shorts".to_string(),
+        },
+    );
+
+    for (locker_number, content) in lockers.iter() {
+        println!("number: {:?}, contents: {:?}", locker_number, content);
+    }
 }
