39e04a7b40c14fb241b29cdc5fcafea8c803d873
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:38:14 +0900

    repeat define

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
+// External Crates allow you to easily use other prople's code
+// within your programs so you can efficiently create new software
+fn main() {}
