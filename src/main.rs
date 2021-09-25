c7154b0539b3af95f6f2ec9e45ee311d67a7d2d1
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:36:14 +0900

    nothing

--- src/main.rs
+++ src/main.rs
@@ -25,7 +25,6 @@
 
 use std::io;
 
-// * Use an enum to store the possible power states
 enum PowerState {
     Off,
     Sleep,
@@ -34,54 +33,44 @@ enum PowerState {
     Hibernate,
 }
 
-// * Use a match expression to convert the user input into the power state enum
 impl PowerState {
-    // use Option because the user can type in anything they want
-    // Option (Some and None)
     fn new(state: &str) -> Option<PowerState> {
         let state = state.trim().to_lowercase();
-        // String -> &str
         match state.as_str() {
             "off" => Some(PowerState::Off),
             "sleep" => Some(PowerState::Sleep),
             "reboot" => Some(PowerState::Reboot),
             "shutdown" => Some(PowerState::Shutdown),
             "hibernate" => Some(PowerState::Hibernate),
-            _ => None
+            _ => None,
         }
     }
 }
 
-// * Use a function with a match expression to print out the power messages
-//   * The function should accept the enum as an input
 fn print_power_action(state: PowerState) {
-    // it uses modules from the standard library and you can also utilize it
-    // for using different items that are available in other parts of code
-    // take everything in PowerState and use in this function
     use PowerState::*;
     match state {
-        Off => println!("Turning off"),
-        Sleep => println!("Sleeping"),
-        Reboot => println!("Rebooting"),
-        Shutdown => println!("Shutting down"),
-        Hibernate => println!("Hibernating"),
+        Off => println!("Turning off.."),
+        Sleep => println!("Sleeping.."),
+        Reboot => println!("Rebooting.."),
+        Shutdown => println!("Shutting down.."),
+        Hibernate => println!("Hibernating.."),
     }
-
 }
 
 fn main() {
-    let mut buffer = String::new();
-    // dont need to use ? after
-    let user_input_status = io::stdin().read_line((&mut buffer));
-    // ok here is defined on results and the read_line returns a result
-    if user_input_status.is_ok() {
-        // the buffer in after main is a owned string and the powerstate new function
-        // require a borrwed
-        match PowerState::new(&buffer) {
-            Some(state) => print_power_action(state),
-            None => println!("Invalid power state"),
+    loop {
+        println!("-------------------");
+        println!("Enter your status..");
+        let mut buffer = String::new();
+        let user_input_status = io::stdin().read_line(&mut buffer);
+        if user_input_status.is_ok() {
+            match PowerState::new(&buffer) {
+                Some(state) => print_power_action(state),
+                None => println!("Invalid power state"),
+            }
+        } else {
+            println!("Error reading input");
         }
-    } else {
-        println!("Error reading input");
     }
 }
