28e6541b064e2013b079fa88cca015cd8644d014
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:38:14 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+// The new types pattern leverages the rust type
+// system in order to make your programs more reliable and easier to manage
+//
+
+
+// typle structure
+#[derive(Debug, Copy, Clone)]
+struct NeverZero(i32);
+
+impl NeverZero {
+    fn new(i: i32) -> Result<Self, String> {}
 }
+
+fn divide(a: i32, b: NeverZero) -> i32 {}
+
+fn main() {}
