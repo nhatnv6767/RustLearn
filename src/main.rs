ae9b43a62674a3fd5ad619b901178e894a4c085d
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:38:14 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+trait Fall {
+    fn hit_ground(&self);
+}
+
+struct Vase;
+impl Fall for Vase {
+    fn hit_ground(&self) {
+        println!("The vase broke")
+    }
+}
+
+struct Cat;
+impl Fall for Cat {
+    fn hit_ground(&self) {
+        println!("The cat casually walked away")
+    }
+}
+
+fn fall(thing: impl Fall) {
+    thing.hit_ground();
+}
 fn main() {
-    println!("Hello, world!");
+    fall(Vase {});
+    fall(Cat {});
 }
