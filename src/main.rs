80a9b619d61ef2aee0df366a616f775167c2b3cf
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:38:14 +0900

    ex

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let maybe_user = Some("Jerry");
+    match maybe_user {
+        Some(user) => println!("user = {:?}", user),
+        None => println!("No user"),
+    }
 }
