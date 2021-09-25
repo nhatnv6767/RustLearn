837caf5e5e4477be27b2835eb14dc11f4f2a824d
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:38:14 +0900

    define and example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+// Iterators are a simple way to traverse and manipulate data structure
+//
 fn main() {
-    println!("Hello, world!");
+    let numbers = vec![1, 2, 3, 4, 5];
+
+    let mut plus_one = vec![];
+    for num in numbers {
+        plus_one.push(num + 1);
+    }
 }
