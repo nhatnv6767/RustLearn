f28610d2fff46c04c858f9b3d4bbc56b529edf22
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-25 21:38:14 +0900

    difine

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-    println!("Hello, world!");
-}
+// fundamentail: Trail
+// . A way to specify that some functionality exists
+// . Used to standardize functionality across multiple different types
+// - Standardization permits functiona to operate on multiple different types
+// - Code deduplication
+fn main() {}
