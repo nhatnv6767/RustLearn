d8c0aa1e024b58594aff790b4b167e6f07ff9ccb
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-27 22:08:11 +0900

    example 2

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,7 @@ fn maybe_word() -> Option<String> {
 
 }
 fn main() {
-    let plus_one = match maybe_num() {
-        // mapping number to be the number + 1
-        Some(num) => Some(num + 1),
-        None => None,
-    };
-
-    // the same meaning, with closures
-    // it's similar to a function here we have our argument to our closure
-    // num + 1 is the body of closure and it's going to automatically
-    // return num + 1
-    // |num| is the inner value of the option
-    let plus_one = maybe_num().map(|num| num + 1);
+    // transformed a string into an integer
+    // the word length will be an optional integer
+    let word_length = maybe_word().map(|word| word.len());
 }
