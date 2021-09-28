0a14fd38812c9efad2b4c6850e451d3eb368ef9f
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-29 00:08:35 +0900

    Hashmap with entry()

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,9 @@ fn main() {
     scores.insert(String::from("Mu"), 10);
     scores.insert(String::from("Mu"), 300);
 
+    // y nghia cua entry: Neu chua co gia tri se insert,
+    // con nhu neu da co gia tri thi se bo qua
+    scores.entry(String::from("MC")).or_insert(10);
+    scores.entry(String::from("MC")).or_insert(200);
     println!("Scores = {:?}", scores);
 }
