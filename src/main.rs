77c03263bdb0345c20e0ef709bc016d821c7d2b9
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 22:25:35 +0900

    cach tao string

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
 fn main() {
-    // Vectors bang cach su dung enum
+    // String: luu o trong heap
+    // UTF-8
+    let s1 = String::from("A String");
+    let s2 = String::new();
+    let s3 = "A String";
 
-    enum SheetCell {
-        Int(i32),
-        Float(f64),
-        Text(String)
-    }
 
-    let row = vec![
-        SheetCell::Int(5),
-        SheetCell::Float(10.12),
-        SheetCell::Text(String::from("Blue")),
-    ];
-
-    match &row[1] {
-        &SheetCell::Float(i) => println!("{}", i),
-        _ => println!("This is not float"),
-    }
 }
