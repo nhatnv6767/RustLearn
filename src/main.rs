8eed04356663eddc64a605e38973b68d516a62d3
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 23:36:49 +0900

    Lenh insert theo kieu in de len gia tri truoc

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,9 @@ use std::collections::HashMap;
 
 fn main() {
     // Hashmap
-    let mu = String::from("MU");
-    let mc = String::from("MC");
-
     let mut scores = HashMap::new();
+    scores.insert(String::from("Mu"), 10);
+    scores.insert(String::from("Mu"), 300);
 
-    scores.insert(mu, 10);
-    scores.insert(mc, 9);
-
-    let team_name = String::from("MU");
-    // get nay la get lay theo index
-    let score = scores.get(&team_name);
-
-    for (key, value) in &scores {
-        println!("{} {}", key, value)
-    }
-
-    println!("Score = {:?}", score);
-
+    println!("Scores = {:?}", scores);
 }
