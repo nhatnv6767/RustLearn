1f4a65b4d53e5c75ab8cf027a0059c45b5cb8255
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 22:48:38 +0900

    some kind else

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,7 @@ fn main() {
     // String: luu o trong heap
     // UTF-8
     let s1 = String::from("A String");
-    let s2 = String::new();
-    let s3 = "A String";
-
-    let s4 = s3.to_string();
-
+    let s2 = String::from("");
+    println!("{}", s2);
 
 }
