73c3559982eacf8eafd8072999f67285565cad52
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-29 00:29:44 +0900

    1 bai toan ve entry rat hay, phu hop cho viec liet ke phan tu va dem tu trong chuoi

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,18 @@ use std::collections::HashMap;
 
 fn main() {
     // Hashmap
-    let mut scores = HashMap::new();
-    scores.insert(String::from("Mu"), 10);
-    scores.insert(String::from("Mu"), 300);
+    let text = "hello world this is wonderful world";
+    let mut map = HashMap::new();
+    // dau tien no se xoa nhung khoang trang
+    // sau do no se duyet qua cac phan tu (chu cai) o trong text do
+    // neu chua co gia tri thi no se insert 0 vao map
+    // sau khi chay xong thi no se tang count len 1
+    // va khi da chay het (tat ca deu co gia tri count = 1) no lai gap thang world 1 lan nua,
+    // no se + 1 them 1 lan nua vao, va khi do ta co world = 2
+    for i in text.split_whitespace() {
+        let count = map.entry(i).or_insert(0);
+        *count += 1;
+    }
 
-    // y nghia cua entry: Neu chua co gia tri se insert,
-    // con nhu neu da co gia tri thi se bo qua
-    scores.entry(String::from("MC")).or_insert(10);
-    scores.entry(String::from("MC")).or_insert(200);
-    println!("Scores = {:?}", scores);
+    println!("{:?}", map);
 }
