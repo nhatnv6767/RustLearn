167e629a18c1d54c8bfae9c0774ac599ca568f08
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 21:09:48 +0900

    vi du ve v.get 2

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,9 @@ fn main() {
 
     let v = vec![1, 2, 3, 4, 5, 6];
 
-    match v.get(20) {
+    let four = &v[3];
+
+    match v.get(3) {
         Some(four) => println!("This is four element = {}", four),
         None => println!("This is not a four element"),
         // se in ra dong None => vi no ko phai element thu 4
