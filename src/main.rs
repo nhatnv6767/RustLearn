7f9e09a6925f1e86128b3af1cb179a8ad70a60e6
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 21:15:58 +0900

    TUong tu nhung nang cao hon

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
 
-    let v = vec![1, 2, 3, 4, 5, 6];
+    let mut v = vec![1, 2, 3, 4, 5, 6];
+
+    for i in &mut v {
+        *i += 10;
+    }
 
     // in ra nhung gia tri trong vector
     for i in &v {
