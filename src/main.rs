4197d83fedc594a1ada89a463faae26fce90f04d
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 21:13:26 +0900

    in gia tri vector

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,8 @@ fn main() {
 
     let v = vec![1, 2, 3, 4, 5, 6];
 
-    let four = &v[3];
-
-    match v.get(20) {
-        Some(four) => println!("This is four element = {}", four),
-        None => println!("This is not a four element"),
-        // se in ra dong None => vi no ko phai element thu 4
+    // in ra nhung gia tri trong vector
+    for i in &v {
+        println!("{}", i)
     }
-
-
 }
