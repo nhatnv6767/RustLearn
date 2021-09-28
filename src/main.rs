630ab9bceffcc8854f6004b6a5c4e805a5536850
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 20:58:39 +0900

    vector nhung giong thang array

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 fn main() {
-    // array
-    let _a: [i32; 3] = [1, 2, 3];
-    // vector
-    let v = vec![1, 2, 3];
-    let mut v2 = Vec::new();
-    v2.push(1);
-    v2.push(2);
-    v2.push(3);
-    v2.push(4);
-    println!("{:?}", v2);
+
+    let v = vec![1, 2, 3, 4, 5, 6];
+
+    // Tham chieu vao no
+    let four = &v[3];
+    println!("four = {}", four)
 
 }
