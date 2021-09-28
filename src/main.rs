3bb188ecc84cdf303b59b4d7cd38d2dbbd966cb1
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-09-28 20:53:45 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ fn main() {
     let _a: [i32; 3] = [1, 2, 3];
     // vector
     let v = vec![1, 2, 3];
-    let v2 = Vec::new();
-    v2.push(32);
+    let mut v2 = Vec::new();
+    v2.push(1);
+    v2.push(2);
+    v2.push(3);
+    v2.push(4);
+    println!("{:?}", v2);
+
 }
