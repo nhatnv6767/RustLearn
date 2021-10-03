083395b3bbd2173dd92783a9abaf4adcb5bc006e
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-04 00:26:04 +0900

    explain it

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,23 @@ trait Noise {
     fn make_noise(&self);
 }
 
+// this is read as the type is something that implement the noise trait within our function body.
+// So this may be an enumeration or struct
+// We don't know -> As long as it implements the noise trait
+// We then call the make_noise func that's defined up in the noise trait, which will then call
+// the make noise function on the appropriate piece of data
 fn hello(noisy: impl Noise) {
     noisy.make_noise();
 }
 
 struct Person;
+// for person structure, we're implementing the noise trait
+// to implement a trait, use the impl keyword and instead of specifying the structure
+// enumeration. You instead specify the name of the trait along with the keyword
+// for and then the name of the structure or enumeration (for Person)
+// Then within the implementation block, you'll list out all of the functions required for that specific
+// trait.
+// in the top, we only have one function -> so you only need to specify one function within the implementation body.
 impl Noise for Person {
     fn make_noise(&self) {
         println!("Hello")
