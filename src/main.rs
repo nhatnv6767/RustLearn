786e8eb3848a0fcbc9e3fa4a7889fa94ba4898fc
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-04 00:05:51 +0900

    example Trait

--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,27 @@
 // you would only need to write one function that operates upon a trait, and
 // then can call that function with multiple different data types as long as they
 // all implement that trait
-fn main() {}
+trait Noise {
+    fn make_noise(&self);
+}
+
+fn hello(noisy: impl Noise) {
+    noisy.make_noise();
+}
+
+struct Person;
+impl Noise for Person {
+    fn make_noise(&self) {
+        println!("Hello")
+    }
+}
+struct Dog;
+impl Noise for Dog {
+    fn make_noise(&self) {
+        println!("woof")
+    }
+}
+fn main() {
+    hello(Person{});
+    hello(Dog {});
+}
