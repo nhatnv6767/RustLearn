867e971d9afd8059822c995ecff6ae0db92c6601
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-03 12:31:28 +0900

    i got mistake

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,17 @@ mod test {
     // a macro, this macro tells the compiler that this function is
     // is testing other code
     #[test]
-    fn check_all_caps(){
+    fn check_all_caps() {
         // asserts_eq will make an assertion that one value is equal to another value
         // and if that assertion does not hold, then the program will abort,
         // thus failing our test
-        #[test]
-        fn test_name() {
-            // three arguments: 1. value want to check
-            // 2. the value that we're expecting
-            // 3. a message we can display when the test does not work out.
-            let result = all_caps("hello");
-            let expected = String::from("HELLO");
-            assert_eq!(result, expected, "String should be all uppercase");
-        }
+
+        // three arguments: 1. value want to check
+        // 2. the value that we're expecting
+        // 3. a message we can display when the test does not work out.
+        let result = all_caps("hello");
+        let expected = String::from("hello");
+        assert_eq!(result, expected, "String should be all uppercase");
     }
 }
 
