de472f0a3d1d63969efc526e529ed020f493d04c
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-04 00:02:04 +0900

    long define

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,7 @@
 // . Used to standardize functionality across multiple different types
 // - Standardization permits functiona to operate on multiple different types
 // - Code deduplication
+// you would only need to write one function that operates upon a trait, and
+// then can call that function with multiple different data types as long as they
+// all implement that trait
 fn main() {}
