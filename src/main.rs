ca6bce30998eb73756619265353230e551608ec1
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-03 11:53:51 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,13 @@
+fn hello() {
+    println!("Hello");
+}
+fn goodbye() {
+    println!("Goodbye");
+}
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
 fn main() {}
