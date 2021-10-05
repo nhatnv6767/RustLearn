c1900ae9409b5cc5233f93d04862916984c9c7e2
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-05 23:37:51 +0900

    if let with enum

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
+enum Color {
+    Red,
+    Blue,
+    Green,
+}
 fn main() {
     let maybe_user = Some("Jerry");
-    match maybe_user {
-        Some(user) => println!("user = {:?}", user),
-        None => println!("No user"),
-    }
 
     if let Some(user) = maybe_user {
         println!("user = {:?}", user);
     } else {
         println!("no user");
+    }
+
+    let red = Color::Red;
+    if let Color::Red = red {
+        println!("Its red!");
+    } else {
+        println!("Its not red");
     }
 }
