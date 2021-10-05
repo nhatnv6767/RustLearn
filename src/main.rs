78b40541d35f54c918cabd13b369dd6b0431c70e
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-05 23:47:32 +0900

    done

--- src/main.rs
+++ src/main.rs
@@ -4,5 +4,11 @@ fn main() {
         println!("Loop");
         data = None;
     }
+
+    let numbers = vec![1, 2, 3];
+    let mut number_iter = numbers.iter();
+    while let Some(num) = number_iter.next() {
+        println!("num = {:?}", num)
+    }
     println!("Done");
 }
