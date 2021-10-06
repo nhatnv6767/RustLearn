ad8902893dff0c3f6e075e2a514432143de65f78
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-06 21:56:47 +0900

    requirements

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-// External Crates allow you to easily use other prople's code
-// within your programs so you can efficiently create new software
+// Topic: External crates
+//
+// Requirements:
+// * Display the current date and time
+//
+// Notes:
+// * Use the `chrono` crate to work with time
+// * (OPTIONAL) Read the documentation section `Formatting and Parsing`
+//   for examples on how to create custom time formats
+
 fn main() {}
