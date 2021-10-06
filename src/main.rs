aaf8015f95e385c9b9df00b172af8ac9c90afab0
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-06 22:33:38 +0900

    example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::default;
+
 // Default trait is used to create new structure
 // in enumerations with a default value.
+
+struct Package {
+    weight: f64,
+}
+
+impl Package {
+    fn new(weight: f64) -> Self {
+        Self { weight }
+    }
+}
+
+// The reason you would want to implement default
+// is because there are other create along with code
+// within the standard library that will attempt to use default
+// when applicable.
+// It's a good idea to implement default for any structure or enumeration where
+// it would make sense to have a default value
+// because it only makes your code easier to use.
+impl Default for Package {
+    fn default() -> Self {
+        Self { weight: 3.0 }
+    }
+}
 fn main() {}
