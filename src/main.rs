78233f8fdfd110c95a8577599f6d37d524f9e922
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-06 22:44:49 +0900

    const

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+const MAX_SPEED: i32 = 9000;
 fn clamp_speed(speed: i32) -> i32 {
-    if speed > 9000 {
-        9000
+    if speed > MAX_SPEED {
+        MAX_SPEED
     } else {
         speed
     }
