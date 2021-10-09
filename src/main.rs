cb2a621be37193571436207c5eb2570703df32f1
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-09 22:02:56 +0900

    check zero value

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,13 @@
 struct NeverZero(i32);
 
 impl NeverZero {
-    fn new(i: i32) -> Result<Self, String> {}
+    fn new(i: i32) -> Result<Self, String> {
+        if i == 0 {
+            Err("cannot be zero".to_string())
+        } else {
+            Ok(Self(i))
+        }
+    }
 }
 
 fn divide(a: i32, b: NeverZero) -> i32 {}
