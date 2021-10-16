4e564738e91cfb4fb64d66ae162c69a768f06170
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-17 01:52:22 +0900

    define

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,24 @@
+// . All data has a memory address
+// - Addresses determine the physical location of data in memory
+// . Offsets can be used to access adjacent addresses
+// - Also called indexes/indices
+
+
+
+// STACK
+// . Data placed sequentially
+// . Limited space
+// . Variables stored on the stack
+// . Very fast to work with
+// - Offsets to access
+
+
+// HEAP
+// . Data placed algorithmically
+// - Slower than stack
+// . Unlimited space (RAM/disk limits apply)
+// . Uses pointers
+// - Pointers are a fixed size
+// - Unsize & isize data type
+// . Vectors & Hashmaps stored on the heap
 fn main() {}
