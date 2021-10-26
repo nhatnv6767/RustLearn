e8a28acbe70a98f743d4529fbf2d036e759a56ea
nhatnguyen6767 <nhatnvse90183@gmail.com>
2021-10-26 23:17:36 +0900

    st

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,12 @@
+// closures: Box
+// The data within the box is surrounded by the <>
+// The data type is a closure as specified Fn,
+// we're passing in a simple style closure,
+// we dont define any variable names for these arguments
+// we only need to supply the data type and then we have the return value
+fn math(a: i32, b: i32, op: Box<Fn(i32, i32) -> i32>) -> i32 {
+    // return operation was performed
+    op(a, b)
+}
+
 fn main() {}
